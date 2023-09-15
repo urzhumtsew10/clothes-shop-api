cbbdd0ab87d9457352d841326b7b3eeca49b469a
andrew <urgumandrei@gmail.com>
2023-09-15 17:18:01 +0200

    user authorization

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "32DWE0EFLTX456D7";
 
 const { MongoClient, ObjectId } = require("mongodb");
 const url =
@@ -36,12 +39,92 @@ const getProducts = async () => {
   }
 };
 
+const getUsers = async () => {
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("clothes-shop");
+    const collection = db.collection("users");
+    const results = await collection.find().toArray();
+    return results;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoClient.close();
+  }
+};
+
+const addNewUser = async (user) => {
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("clothes-shop");
+    const collection = db.collection("users");
+    const results = await collection.insertOne(user);
+    return results;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoClient.close();
+  }
+};
+
 app.get("/", (req, res) => {
   res.json("Hello world");
 });
 
 app.post("/service", (req, res) => {
   getProducts().then((products) => res.send(JSON.stringify(products)));
+});
+
+app.post("/find-user", (req, res) => {
+  const reqUser = req.body;
+
+  getUsers().then((users) => {
+    const findUser = users.filter(
+      (user) => user.name === reqUser.name && user.password === reqUser.password
+    );
+    if (findUser.length === 0) {
+      res.send(JSON.stringify({ isFound: false }));
+    } else {
+      const token = jwt.sign({ name: reqUser.name }, SECRET_KEY);
+      res.send(
+        JSON.stringify({ isFound: true, user: findUser[0], token: token })
+      );
+    }
+  });
+});
+
+app.post("/create-user", (req, res) => {
+  const userData = req.body;
+
+  getUsers().then((users) => {
+    const checkNameUser = users.filter((user) => user.name === userData.name);
+    const checkEmailUser = users.filter(
+      (user) => user.email === userData.email
+    );
+
+    const response = {};
+
+    if (checkNameUser.length !== 0) {
+      response.name = false;
+    } else {
+      response.name = true;
+    }
+
+    if (checkEmailUser.length !== 0) {
+      response.email = false;
+    } else {
+      response.email = true;
+    }
+    const token = jwt.sign({ name: userData.name }, SECRET_KEY);
+    res.send(JSON.stringify({ ...response, token: token }));
+  });
+});
+
+app.post("/add-user", (req, res) => {
+  const newUser = req.body;
+  const token = jwt.sign({ name: userData.name }, SECRET_KEY);
+
+  addNewUser({ ...newUser, role: "user", token: token });
 });
 
 app.post("/sort", (req, res) => {
